4ad2a60da13c01c7ee2bd9a76b19ed39637c99a7
MMAndresC <mandrescasabon@hotmail.com>
2024-02-02 19:52:56 +0100

    changed detection enemy checks

--- Main.java
+++ Main.java
@@ -11,14 +11,23 @@ public class Main {
 		int[] coordMov = new int[2];
 		boolean valido = false;
 		boolean rendirse = false;
-		char charNumerojugador;
+		char charNumerojugador, enemigo;
 		char opcion = ' ';
+		int siguienteLinea = -1, puedeMatar = -1;
+		int linea = -1, cDerecha = -1, cIzquierda = -1;
+		int[] coordDerecha = new int[2];
+		int[] coordIzquierda = new int[2];
+		coordIzquierda[0] = -1;
+		coordIzquierda[1] = -1;
+		coordDerecha[0] = -1;
+		coordDerecha[1] = -1;
 		if(player.getNumero() == 1) {
 			charNumerojugador = '1';
+			enemigo = '2';
 		}else{
 			charNumerojugador = '2';
+			enemigo = '1';
 		}
-		int linea, cDerecha, cIzquierda;
 		System.out.println();
 		System.out.println();
 		System.out.println("Turno del jugador " + player.getNumero());
@@ -34,8 +43,10 @@ public class Main {
 				if(tablero.getTablero()[damaSeleccionada[0]][damaSeleccionada[1]] == charNumerojugador) {
 					if(player.getNumero() == 1) {
 						linea = damaSeleccionada[0] + 1;
+						siguienteLinea = damaSeleccionada[0] + 2 > 7 ? damaSeleccionada[0] + 2 : -1;
 					}else {
 						linea = damaSeleccionada[0] - 1;
+						siguienteLinea = damaSeleccionada[0] - 2 < 0 ? damaSeleccionada[0] - 2 : -1;
 					}
 					if(damaSeleccionada[1] - 1 >= 0) {
 						cIzquierda = damaSeleccionada[1] - 1;
@@ -44,30 +55,50 @@ public class Main {
 						cDerecha = damaSeleccionada[1] + 1;
 					}
 					if(cDerecha != -1) {
-						if(tablero.getTablero()[linea][cDerecha] == 'L') valido = true;
+						if(tablero.getTablero()[linea][cDerecha] == 'L'){
+							valido = true;
+							coordDerecha[0] = linea;
+							coordDerecha[1] = cDerecha;
+						}else if(tablero.getTablero()[linea][cDerecha] == enemigo && siguienteLinea != -1)
+							if(cDerecha + 1 <= 7){
+								if(tablero.getTablero()[siguienteLinea][cDerecha + 1] == 'L'){
+									valido= true;
+									puedeMatar = cDerecha;
+									coordDerecha[0] = siguienteLinea;
+									coordDerecha[1] = cDerecha;
+								}
+							}
+						}
 					}
 					if(cIzquierda != -1) {
-						if(tablero.getTablero()[linea][cIzquierda] == 'L') valido = true;
+						if(tablero.getTablero()[linea][cIzquierda] == 'L') {
+							valido = true;
+							coordIzquierda[0] = linea;
+							coordIzquierda[1] = cIzquierda;
+						}else if(tablero.getTablero()[linea][cIzquierda] == enemigo && siguienteLinea != -1){
+							if(cIzquierda - 1 >= 0){
+								if(tablero.getTablero()[siguienteLinea][cIzquierda - 1] == 'L'){
+									valido = true;
+									puedeMatar = cIzquierda;
+									coordIzquierda[0] = siguienteLinea;
+									coordIzquierda[1] = cIzquierda;
+								}
+							}
+						}
 					}
-				}
+
 				if(!valido) System.out.println("ERROR ficha mal seleccionada");
 			}while(!valido);
 			valido = false;
-			cIzquierda = -1;
-			cDerecha = -1;
+			System.out.print(coordDerecha[0]);System.out.print(coordDerecha[1]);
+			System.out.print(coordIzquierda[0]);System.out.print(coordIzquierda[1]);
 			do {
-				if(player.getNumero() == 1) {
-					if((damaSeleccionada[0] + 1) < 8) coordMov[0] = damaSeleccionada[0] + 1;
-				}
-				if(player.getNumero() == 2) {
-					if((damaSeleccionada[0] - 1) >=0) coordMov[0] = damaSeleccionada[0] - 1;
-				}
-				
 				System.out.print("Columna a la que quieres mover: ");
 				coordMov[1] = Game.leerCoordenadas();
-				if(damaSeleccionada[1] - 1 >= 0) cIzquierda = damaSeleccionada[1] - 1;
-				if(damaSeleccionada[1] + 1 <= 7) cDerecha = damaSeleccionada[1] + 1;
-				if(coordMov[1] == cIzquierda || coordMov[1] == cDerecha)valido = true;
+				if(coordMov[1] == coordDerecha[1] || coordMov[1] == coordIzquierda[1]){
+					valido = true;
+					coordMov[0] = coordMov[1] == coordDerecha[1] ? coordDerecha[0] : coordIzquierda[0];
+				}
 				if(!valido) System.out.println("Error, movimiento no permitido");
 			}while(!valido);
 		
@@ -97,7 +128,7 @@ public class Main {
 			if(player1.isTurno()) {
 				rendirse = controlMov(player1, tablero);
 				if(!rendirse) {
-					checkSituation(tablero, player1, player2);
+					//checkSituation(tablero, player1, player2);
 					tablero.MostrarTablero();
 					System.out.println("Fichas comidas: " + player1.getDamasMuertas());
 					player2.setTurno(true);
@@ -108,7 +139,7 @@ public class Main {
 			}else {
 				rendirse = controlMov(player2, tablero);
 				if(!rendirse) {
-					checkSituation(tablero, player2, player1);
+					//checkSituation(tablero, player2, player1);
 					tablero.MostrarTablero();
 					System.out.println("Fichas comidas: " + player2.getDamasMuertas());
 					player1.setTurno(true);
@@ -125,20 +156,20 @@ public class Main {
 
 	public static void checkSituation(Tableboard board, Player player, Player other) {
 		int linea = -1, siguienteLinea = 0, cIzquierda = -1, cDerecha = -1;
-		char enemy = ' ', element =' ';
+		char enemigo = ' ', elemento =' ';
 		boolean puedeMatar = false;
 		linea = player.getMovLinea();
 		if(player.getNumero() == 1 && (player.getMovLinea() + 2) < 8 ) {
 			linea++;
 			siguienteLinea = linea + 1;
-			element = '1';
-			enemy = '2';
+			elemento = '1';
+			enemigo = '2';
 		}
 		if(player.getNumero() == 2 && (player.getMovLinea() - 2) >= 0 ) {
 			linea--;
 			siguienteLinea = linea - 1;
-			element = '2';
-			enemy = '1';
+			elemento = '2';
+			enemigo = '1';
 		} 
 		if(player.getMovColumna() - 2 >= 0) {
 			cIzquierda = player.getMovColumna() - 1;
@@ -146,25 +177,26 @@ public class Main {
 		if(player.getMovColumna() + 2 <= 7) {
 			cDerecha = player.getMovColumna() + 1;
 		}
-		if(cIzquierda != -1 && linea != -1 && (board.getTablero()[linea][cIzquierda]) == enemy ) {
+
+		if(cIzquierda != -1 && linea != -1 && (board.getTablero()[linea][cIzquierda]) == enemigo ) {
 			if(board.getTablero()[siguienteLinea][cIzquierda - 1] == 'L') {
 				puedeMatar = true;
-				board.modificarElementoTablero(siguienteLinea ,cIzquierda - 1, element);
+				board.modificarElementoTablero(siguienteLinea ,cIzquierda - 1, elemento);
 				board.modificarElementoTablero(player.getMovLinea(),player.getMovColumna(), 'L');
 				board.modificarElementoTablero(linea, cIzquierda, 'L');
 				other.setDamasQuedan(other.getDamasQuedan() - 1);
 				player.setDamasMuertas(player.getDamasMuertas() + 1);
 			}
 		}
-		if(cDerecha != -1 && !puedeMatar && linea != -1 && (board.getTablero()[linea][cDerecha]) == enemy ) {
+		if(cDerecha != -1 && !puedeMatar && linea != -1 && (board.getTablero()[linea][cDerecha]) == enemigo ) {
 			if(board.getTablero()[siguienteLinea][cDerecha + 1] == 'L') {
 				puedeMatar = true;
-				board.modificarElementoTablero(siguienteLinea ,cDerecha + 1, element);
+				board.modificarElementoTablero(siguienteLinea ,cDerecha + 1, elemento);
 				board.modificarElementoTablero(player.getMovLinea(),player.getMovColumna(), 'L');
 				board.modificarElementoTablero(linea, cDerecha, 'L');
 				other.setDamasQuedan(other.getDamasQuedan() - 1);
 				player.setDamasMuertas(player.getDamasMuertas() + 1);
 			}
-		}	
+		}
 	}
 }
